3fa6246ad782ba0d86bef85b93c8f1ee3bcf77ab
Victor Wade <p9859817888@gmail.com>
2024-09-12 04:38:59 -1000

    functional page UI

--- js/index.js
+++ js/index.js
@@ -18,16 +18,18 @@ const flavors = [
 	"Coconut",
 	"Raspberry",
 ];
-const random_flavors = Array(Math.round(4 * (1 + Math.random() * 2))) // between 4 and 12 items
-	.fill()
-	.map((e) => {
-		// create a randomized list of default flavors
-		const rand_idx = Math.floor(Math.random() * flavors.length);
-		return flavors[rand_idx];
-	})
-	.join(", ");
+const random_flavors = function (e) {
+	return Array(Math.round(4 * (1 + Math.random() * 2))) // between 4 and 12 items
+		.fill()
+		.map((e) => {
+			// create a randomized list of default flavors
+			const rand_idx = Math.floor(Math.random() * flavors.length);
+			return flavors[rand_idx];
+		})
+		.join(", ");
+};
 
-const user_input = prompt(welcome_message, random_flavors)
+const user_input = prompt(welcome_message, random_flavors())
 	.split(",")
 	.map((e) => e.trim());
 
@@ -51,18 +53,63 @@ const flavor_count = count_orders(user_input);
 console.log("Order counts", flavor_count);
 
 function create_page_elements() {
+	const div_form = document.createElement("div");
+	const form = document.createElement("form");
+
+	const form_input = document.createElement("input");
+	form_input.setAttribute("type", "text");
+	form_input.setAttribute("placeholder", "Enter as many flavors as you want");
+	form_input.value = user_input.join(", ");
+
+	// Submit button
+	const form_button = document.createElement("button");
+	form_button.setAttribute("type", "submit");
+	form_button.textContent = "Submit Order";
+
+	form.append(form_input, form_button);
+	div_form.append(form);
+
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
+
+		const user_input = form_input.value.split(",").map((e) => e.trim());
+
+		if (0 == user_input.length) {
+			if (confirm("Would you like a few random suggestions?")) {
+				form_input.value = random_flavors();
+			}
+		}
+		if (0 < user_input.length) {
+			div_result_container.innerHTML = "";
+
+			const flavor_count = count_orders(user_input);
+
+			for (const [flavor, count] of Object.entries(flavor_count)) {
+				const p = document.createElement("p");
+				p.innerText = `${flavor}: ${count}`;
+				div_result_container.append(p);
+			}
+		}
+	});
+
 	const div_result = document.createElement("div");
 	const div_result_h2 = document.createElement("h2");
+	const div_result_container = document.createElement("div");
 	div_result_h2.innerText = "Flavor Counts: ";
-	div_result.append(div_result_h2);
+	div_result.append(form, div_result_h2, div_result_container);
+
+	div_result_container.id = "orders";
 
 	for (const [flavor, count] of Object.entries(flavor_count)) {
 		const p = document.createElement("p");
 		p.textContent = `${flavor}: ${count}`;
-		div_result.appendChild(p);
+		div_result_container.appendChild(p);
 	}
 
-	document.body.appendChild(div_result);
+	const body = document.createElement("body");
+
+	body.appendChild(div_result);
+	document.body = body;
 }
 
 // put info to page
